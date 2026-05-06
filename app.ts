24b8b17f481aeaba9b6ecdecd489a32917e6f9a0
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-05 22:06:24 -0500

    feat(app): add userRoutes

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { setupSwagger } from "./src/shared/infrastructure/documentation/swagger";
+import userRoutes from "./src/context/iam/interfaces/routes/UserRoutes";
 
 const app = express();
 
 app.use(express.json());
 
 setupSwagger(app);
+
+app.use("/api/users", userRoutes)
 
 export default app;
