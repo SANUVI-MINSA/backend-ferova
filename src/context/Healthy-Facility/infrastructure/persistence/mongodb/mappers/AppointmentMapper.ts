186d9b52cf33b276557f8f58da783636f9cf2968
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 12:38:20 -0500

    feat(infrastructure): add mappers Appointment and NurseAssigment

---
+++ src/context/Healthy-Facility/infrastructure/persistence/mongodb/mappers/AppointmentMapper.ts
@@ -0,0 +1,29 @@
+import {Appointment} from "../../../../domain/model/entities/Appointment";
+
+export class AppointmentMapper {
+    static toDomain(document: any): Appointment {
+        return new Appointment(
+            document.id,
+            document.facilityId,
+            document.patientId,
+            document.motherId,
+            document.nurseId,
+            document.date,
+            document.status
+        )
+    }
+
+    static toPersistence(appointment: Appointment) {
+        const data = appointment.toPrimitives();
+
+        return {
+            id: data.id,
+            facilityId: data.facilityId,
+            patientId: data.patientId,
+            motherId: data.motherId,
+            nurseId: data.nurseId,
+            date: data.date,
+            status: data.status
+        }
+    }
+}
