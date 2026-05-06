d1cc385b8663983c25dc8b9ba3170be2e4b7a74b
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 10:47:22 -0500

    refactor(queries): add queries to List Facility, Get History Appointment with Id Patient and GetHealthFacilityDetailQuery.

---
+++ src/context/Healthy-Facility/domain/model/queries/GetPatientAppointmentHistoryQuery.ts
@@ -0,0 +1,3 @@
+export type GetPatientAppointmentHistoryQuery = Readonly<{
+    patientId: string;
+}>;
---
+++ src/context/Healthy-Facility/domain/model/queries/ListHealthFacilitiesQuery.ts
@@ -0,0 +1,4 @@
+export type ListHealthFacilitiesQuery = Readonly<{
+    userLatitude: number;
+    userLongitude: number;
+}>
