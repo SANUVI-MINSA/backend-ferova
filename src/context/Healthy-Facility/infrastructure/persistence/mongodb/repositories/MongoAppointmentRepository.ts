f42b4d79c5b4091e8a305286654406908a9b0e0a
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 13:41:50 -0500

    feat(repositories): implement Repositories for Healthy management operation to MongoRepositories

---
+++ src/context/Healthy-Facility/infrastructure/persistence/mongodb/repositories/MongoAppointmentRepository.ts
@@ -0,0 +1,69 @@
+import {AppointmentRepository} from "../../../../domain/repositories/AppointmentRepository";
+import {Appointment} from "../../../../domain/model/entities/Appointment";
+import {AppointmentMapper} from "../../../mappers/AppointmentMapper";
+import {AppointmentModel} from "../models/AppointmentModel";
+
+export class MongoAppointmentRepository implements AppointmentRepository {
+
+    async findByFacilityAndDateTime(
+        facilityId: string,
+        appointmentDate: string,
+        appointmentTime: string
+    ): Promise<Appointment | null> {
+
+        const appointment =
+            await AppointmentModel.findOne({
+                facilityId,
+                appointmentDate,
+                appointmentTime
+            });
+
+        if (!appointment) {
+            return null;
+        }
+
+        return AppointmentMapper
+            .toDomain(appointment);
+    }
+
+    async findByPatientId(patientId: string): Promise<Appointment[]> {
+        const appointments =
+            await AppointmentModel.find({
+                patientId
+            });
+
+        return appointments.map(
+            appointments =>
+                AppointmentMapper
+                    .toDomain(appointments)
+        );
+    }
+
+    async save(appointment: Appointment): Promise<Appointment> {
+
+        const data =
+            AppointmentMapper
+                .toPersistence(appointment);
+
+        const createAppointment =
+            await AppointmentModel.create(
+                data)
+        ;
+
+        return AppointmentMapper
+            .toDomain(createAppointment);
+    }
+
+    async update(appointment: Appointment): Promise<void> {
+        const data =
+            AppointmentMapper.toPersistence(appointment);
+
+        await AppointmentModel.updateOne(
+            {
+                id: data.id,
+            },
+            data
+        )
+    }
+
+}
