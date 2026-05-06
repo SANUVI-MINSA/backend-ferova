09711bcb1909afcb932ccd4c650bdc2dac8c2f88
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-05 22:03:26 -0500

    feat(iam): add UserFacade for user registration, login, and password management

---
+++ src/context/iam/interfaces/acl/facade/UserFacade.ts
@@ -0,0 +1,44 @@
+import {UserCommandService} from "../../../domain/services/UserCommandService";
+import {UserQueryService} from "../../../domain/services/UserQueryService";
+
+export class UserFacade {
+
+    constructor(
+        private commandService: UserCommandService,
+        private queryService: UserQueryService
+    ) {}
+
+    async registerMother(data: any): Promise<void> {
+        await this.commandService.registerMother(data);
+    }
+
+    async createStaffUser(data: any): Promise<void> {
+        await this.commandService.createStaffUser(data);
+    }
+
+    async login(data: any): Promise<string> {
+        return this.commandService.login(data);
+    }
+
+    async getUserById(userId: string) {
+        return this.queryService.getUserById({
+            userId
+        });
+    }
+
+    async requestResetCode(
+        data: any
+    ): Promise<void> {
+        await this.commandService.requestResetCode(
+            data
+        );
+    }
+
+    async resetPassword(
+        data: any
+    ): Promise<void> {
+        await this.commandService.resetPassword(
+            data
+        );
+    }
+}
