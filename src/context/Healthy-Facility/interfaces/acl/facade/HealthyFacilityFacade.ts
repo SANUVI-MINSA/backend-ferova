52b01e8ecbc0ca444370837510a911577ecca2df
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 22:53:59 -0500

    feat(controller): implement HealthFacilityController with CRUD operations for health facilities

---
+++ src/context/Healthy-Facility/interfaces/acl/facade/HealthyFacilityFacade.ts
@@ -0,0 +1,133 @@
+import {HealthyFacilityCommandService} from "../../../domain/services/HealthyFacilityCommandService";
+import {RegisterHealthFacilityCommand} from "../../../domain/model/commands/RegisterHealthFacilityCommand";
+import {AssignNurseToFacilityCommand} from "../../../domain/model/commands/AssignNurseToFacilityCommand";
+import {BookAppointmentCommand} from "../../../domain/model/commands/BookAppointmentCommand";
+import {CancelAppointmentCommand} from "../../../domain/model/commands/CancelAppointmentCommand";
+import {ListHealthFacilitiesQuery} from "../../../domain/model/queries/ListHealthFacilitiesQuery";
+import {GetPatientAppointmentHistoryQuery} from "../../../domain/model/queries/GetPatientAppointmentHistoryQuery";
+import {GetHealthFacilityDetailQuery} from "../../../domain/model/queries/GetHealthFacilityDetailQuery";
+import {HealthFacilityQueryService} from "../../../domain/services/HealthyFacilityQueryService";
+import {GetNurseAppointmentScheduleQuery} from "../../../domain/model/queries/GetNurseAppointmentScheduleQuery";
+import {Appointment} from "../../../domain/model/entities/Appointment";
+import {GetFacilityAvailableSlotsQuery} from "../../../domain/model/queries/GetFacilityAvailableSlotsQuery";
+import {GetMotherNextAppointmentQuery} from "../../../domain/model/queries/GetMotherNextAppointmentQuery";
+
+export class HealthFacilityFacade {
+
+    constructor(
+        private commandService:
+        HealthyFacilityCommandService,
+
+        private queryService:
+        HealthFacilityQueryService,
+    ) {}
+
+    async registerHealthFacility(
+        command:
+        RegisterHealthFacilityCommand
+    ): Promise<void> {
+
+        await this.commandService
+            .registerFacility(
+                command
+            );
+    }
+
+    async assignNurseToFacility(
+        command:
+        AssignNurseToFacilityCommand
+    ): Promise<void> {
+
+        await this.commandService
+            .assignNurseToFacility(
+                command
+            );
+    }
+
+    async bookAppointment(
+        command:
+        BookAppointmentCommand
+    ): Promise<void> {
+
+        await this.commandService
+            .bookAppointment(
+                command
+            );
+    }
+
+    async cancelAppointment(
+        command:
+        CancelAppointmentCommand
+    ): Promise<void> {
+
+        await this.commandService
+            .cancelAppointment(
+                command
+            );
+    }
+
+    async listHealthFacilities(
+        query:
+        ListHealthFacilitiesQuery
+    ) {
+
+        return await this.queryService
+            .listHealthFacilities(
+                query
+            );
+    }
+
+    async getHealthFacilityDetail(
+        query:
+        GetHealthFacilityDetailQuery
+    ) {
+
+        return await this.queryService
+            .getHealthFacilityDetail(
+                query
+            );
+    }
+
+    async getPatientAppointmentHistory(
+        query:
+        GetPatientAppointmentHistoryQuery
+    ) {
+
+        return await this.queryService
+            .getPatientAppointmentHistory(
+                query
+            );
+    }
+
+    async getNurseAppointmentSchedule(
+        query: GetNurseAppointmentScheduleQuery
+    ): Promise<Appointment[]> {
+
+        return await this.queryService
+            .getNurseAppointmentSchedule(
+                query
+            );
+    }
+
+    async getFacilityAvailableSlots(
+        query: GetFacilityAvailableSlotsQuery
+    ): Promise<any[]> {
+
+        return await this
+            .queryService
+            .getFacilityAvailableSlots(
+                query
+            );
+    }
+
+    async getMotherNextAppointment(
+        query: GetMotherNextAppointmentQuery
+    ): Promise<any>{
+
+        return await this
+            .queryService
+            .getMotherNextAppointment(
+                query
+            );
+    }
+}
