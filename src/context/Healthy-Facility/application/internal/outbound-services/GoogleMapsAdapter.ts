0d4f3ee27643efa8d7f036c9cf0370a6ef13ab32
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 22:35:27 -0500

    feat: remove all adapter of google Service

--- src/context/Healthy-Facility/application/internal/outbound-services/GoogleMapsAdapter.ts
+++
@@ -1,9 +0,0 @@
-export interface GoogleMapsAdapter {
-
-    calculateDistance(
-        originLat: number,
-        originLng: number,
-        destinationLat: number,
-        destinationLng: number
-    ): Promise<number>;
-}
