91f60ba20e05fa464b4c069fca9759b03a26c13f
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 22:54:54 -0500

    feat(command): add route     "/api/health-facilities",

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { setupSwagger } from "./src/shared/infrastructure/documentation/swagger";
+import healthFacilityRoutes from "./src/context/Healthy-Facility/interfaces/routes/HealthFacilityRoutes";
 
 const app = express();
 
 app.use(express.json());
 
 setupSwagger(app);
+
+app.use(
+    "/api/health-facilities",
+    healthFacilityRoutes
+);
 
 export default app;
