bc116e7d1ddef07be693ecaa43fbd0816669a812
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-06 22:54:34 -0500

    feat(command): update RegisterHealthFacilityCommand by removing districtName and scheduleOfOperation fields

--- src/context/Healthy-Facility/domain/model/commands/RegisterHealthFacilityCommand.ts
+++ src/context/Healthy-Facility/domain/model/commands/RegisterHealthFacilityCommand.ts
@@ -2,12 +2,10 @@ export type RegisterHealthFacilityCommand = Readonly<{
     name: string;
     address: string;
     districtId: string;
-    districtName: string;
     latitude: number;
     longitude: number;
     phoneNumber: string;
     services: string[];
     availableDays: string[];
     availableSlots: string[];
-    scheduleOfOperation: string;
 }>;
