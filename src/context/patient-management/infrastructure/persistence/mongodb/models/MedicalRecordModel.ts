acfd08c6cd372672b234652f0c1ad79cd000e29e
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-07 03:37:29 -0500

    feat(models): add model schema mongo patient and medical record

---
+++ src/context/patient-management/infrastructure/persistence/mongodb/models/MedicalRecordModel.ts
@@ -0,0 +1,104 @@
+import mongoose from "mongoose";
+
+const controlSchema =
+    new mongoose.Schema({
+        id: String,
+
+        date: Date,
+
+        hemoglobinLevel:
+        Number,
+
+        anemiaStatus:
+        String
+    });
+
+const antecedenteSchema =
+    new mongoose.Schema({
+        type: String,
+        description: String
+    });
+
+const medicalRecordSchema =
+    new mongoose.Schema(
+        {
+            id: {
+                type: String,
+                required: true,
+                unique: true
+            },
+
+            patientId: {
+                type: String,
+                required: true
+            },
+
+            nurseId: {
+                type: String,
+                required: true
+            },
+
+            createdAt: {
+                type: Date,
+                required: true
+            },
+
+            updatedAt: {
+                type: Date,
+                required: true
+            },
+
+            hemoglobinLevel: {
+                type: Number,
+                required: true
+            },
+
+            weight: {
+                type: Number,
+                required: true
+            },
+
+            height: {
+                type: Number,
+                required: true
+            },
+
+            gender: {
+                type: String,
+                required: true
+            },
+
+            antecedentes:
+                [antecedenteSchema],
+
+            motivoConsulta: {
+                type: String,
+                required: true
+            },
+
+            observaciones: {
+                type: String,
+                default: null
+            },
+
+            sintomas: [
+                {
+                    type: String
+                }
+            ],
+
+            nusrel: {
+                type: String,
+                required: true
+            },
+
+            controls:
+                [controlSchema]
+        }
+    );
+
+export const MedicalRecordModel =
+    mongoose.model(
+        "MedicalRecord",
+        medicalRecordSchema
+    );
