37887d378d98357ba3006a17b68978eb1cb667d8
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-08 08:58:46 -0500

    feat(nutritional-diary): add nutritional diary routes to the main application

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import { setupSwagger } from "./src/shared/infrastructure/documentation/swagger"
 import healthFacilityRoutes from "./src/context/Healthy-Facility/interfaces/routes/HealthFacilityRoutes";
 import userRoutes from "./src/context/iam/interfaces/routes/UserRoutes";
 import patientRoutes from "./src/context/patient-management/interfaces/routes/PatientManagementRoutes";
+import nutritionalDiaryRoutes from "./src/context/nutration-diary/interfaces/routes/NutritionalDiaryRoutes";
 
 const app = express();
 
@@ -19,5 +20,10 @@ app.use("/api/users", userRoutes)
 app.use(
     "/api/patients",
     patientRoutes
+);
+
+app.use(
+    "/api/nutritional-diary",
+    nutritionalDiaryRoutes
 );
 export default app;
