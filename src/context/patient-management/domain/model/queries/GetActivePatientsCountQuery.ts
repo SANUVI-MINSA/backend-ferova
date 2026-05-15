96bac06911f9a69da713a283ad57b09bcb201ed1
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-15 12:10:20 -0500

    feat(queries): add types for active patients count and mother patients summary

---
+++ src/context/patient-management/domain/model/queries/GetActivePatientsCountQuery.ts
@@ -0,0 +1,4 @@
+export type GetActivePatientsCountQuery  =
+    Readonly<{
+        nurseId: string;
+    }>;
---
+++ src/context/patient-management/domain/model/queries/GetMotherPatientsSummaryQuery.ts
@@ -0,0 +1,3 @@
+export type GetMotherPatientsSummaryQuery  = Readonly<{
+    motherId: string;
+}>;
