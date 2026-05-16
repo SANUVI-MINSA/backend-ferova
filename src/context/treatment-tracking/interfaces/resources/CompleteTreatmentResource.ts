60385597fae5a24ce598cba85cbd444be31d5029
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-16 04:13:55 -0500

    feat(resources): add treatment and dose resource interfaces for management

---
+++ src/context/treatment-tracking/interfaces/resources/CompleteTreatmentResource.ts
@@ -0,0 +1,5 @@
+export interface CompleteTreatmentResource {
+    treatmentId: string;
+    nurseId: string;
+    observation?: string;
+}
---
+++ src/context/treatment-tracking/interfaces/resources/ConfirmDoseResource.ts
@@ -0,0 +1,6 @@
+export interface ConfirmDoseResource {
+    treatmentId: string;
+    patientId: string;
+    motherId: string;
+    dailyDoseId: string;
+}
