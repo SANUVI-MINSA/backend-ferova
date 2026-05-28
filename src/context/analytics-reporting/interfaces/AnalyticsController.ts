1e3c83a8629dbcf168b6ffda7686a581ac8e578a
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-27 19:06:22 -0500

    feat(controller): add AnalyticsController manages endpoint

---
+++ src/context/analytics-reporting/interfaces/AnalyticsController.ts
@@ -0,0 +1,45 @@
+import {AnalyticsQueryService} from "../domain/services/AnalyticsQueryService";
+import { Request, Response } from "express";
+import {AuthRequest} from "../../../middlewares/auth.middleware";
+
+export class AnalyticsController {
+
+    constructor(
+        private analyticsQueryService: AnalyticsQueryService
+    ) {}
+
+    getDashboardSummary = async (req: AuthRequest, res: Response) => {
+        try {
+            const result = await this.analyticsQueryService.getDashboardSummary({});
+            res.status(200).json(result);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+    };
+
+    getFacilitiesAnalytics = async (req: AuthRequest, res: Response) => {
+        try {
+            const riskLevelFilter = req.query.riskLevel as "LOW" | "MEDIUM" | "HIGH" | undefined;
+
+            const result = await this.analyticsQueryService.getFacilitiesAnalytics({
+                riskLevelFilter
+            });
+            res.status(200).json(result);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+    };
+
+    getFacilityHeatmapData = async (req: AuthRequest, res: Response) => {
+        try {
+            const riskLevelFilter = req.query.riskLevel as "LOW" | "MEDIUM" | "HIGH" | undefined;
+
+            const result = await this.analyticsQueryService.getFacilityHeatmapData({
+                riskLevelFilter
+            });
+            res.status(200).json(result);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+    };
+}
