7c95fadd48c8fec678dbbc67e47aeda00eaaf96a
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-27 19:07:15 -0500

    feat(controller): add AnalyticsQueryServiceImpl to queries implements getDashboardSummary, getFacilitiesAnalytics, getFacilityHeatmapData

---
+++ src/context/analytics-reporting/application/services/AnalyticsQueryServiceImpl.ts
@@ -0,0 +1,33 @@
+import {AnalyticsQueryService} from "../../domain/services/AnalyticsQueryService";
+import {MongoAnalyticsRepository} from "../../infrastructure/persistence/mongodb/repositories/MongoAnalyticsRepository";
+import {GetDashboardSummaryQuery} from "../../domain/model/queries/GetDashboardSummaryQuery";
+import {DashboardSummaryResponseDto} from "../dto/DashboardSummaryResponseDto";
+import {GetFacilitiesAnalyticsQuery} from "../../domain/model/queries/GetFacilitiesAnalyticsQuery";
+import {FacilitiesAnalyticsResponseDto} from "../dto/FacilityAnalyticsItemDto";
+import {GetFacilityHeatmapDataQuery} from "../../domain/model/queries/GetFacilityHeatmapDataQuery";
+import {HeatmapDataResponseDto} from "../dto/HeatmapPointDto";
+
+export class AnalyticsQueryServiceImpl implements AnalyticsQueryService {
+
+    constructor(
+        private analyticsRepository: MongoAnalyticsRepository
+    ) {}
+
+    async getDashboardSummary(
+        query: GetDashboardSummaryQuery
+    ): Promise<DashboardSummaryResponseDto> {
+        return await this.analyticsRepository.getDashboardSummary();
+    }
+
+    async getFacilitiesAnalytics(
+        query: GetFacilitiesAnalyticsQuery
+    ): Promise<FacilitiesAnalyticsResponseDto> {
+        return await this.analyticsRepository.getFacilitiesAnalytics(query.riskLevelFilter);
+    }
+
+    async getFacilityHeatmapData(
+        query: GetFacilityHeatmapDataQuery
+    ): Promise<HeatmapDataResponseDto> {
+        return await this.analyticsRepository.getFacilityHeatmapData(query.riskLevelFilter);
+    }
+}
