e478df214cac8a5f5147b9046c63808d8f16ed37
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-29 11:20:59 -0500

    feat: add MongoConnection

--- src/shared/infrastructure/persistence/mongodb/MongoConnection.ts
+++ src/shared/infrastructure/persistence/mongodb/MongoConnection.ts
@@ -1,22 +1,19 @@
+// src/shared/infrastructure/persistence/mongodb/MongoConnection.ts
 import mongoose from "mongoose";
+import { env } from "../../config/env";
 
 export class MongoConnection {
 
     static async connect(): Promise<void> {
         try {
-            await mongoose.connect(
-                process.env.MONGO_URI!
-            );
+            await mongoose.connect(env.mongoUri);
 
-            console.log(
-                "MongoDB connected successfully"
-            );
+            console.log(`MongoDB connected successfully to: ${env.mongoUri}`);
+            console.log(`Database: ${mongoose.connection.db?.databaseName}`);
 
         } catch (error) {
-            console.error(
-                "Mongo connection error",
-                error
-            );
+            console.error("Mongo connection error", error);
+            process.exit(1);
         }
     }
 }
