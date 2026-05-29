d06e7e1bda0b18928d2268c170d6fde1c9f0bbb6
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-28 23:05:14 -0500

    feat: add new getTopFacilities in Controller

--- src/context/analytics-reporting/interfaces/AnalyticsController.ts
+++ src/context/analytics-reporting/interfaces/AnalyticsController.ts
@@ -42,4 +42,14 @@ export class AnalyticsController {
             res.status(400).json({ error: error.message });
         }
     };
+
+    getTopFacilities = async (req: AuthRequest, res: Response) => {
+        try {
+            const result = await this.analyticsQueryService.getTopFacilities({});
+            res.status(200).json(result);
+        } catch (error: any) {
+            res.status(400).json({ error: error.message });
+        }
+    };
+
 }
