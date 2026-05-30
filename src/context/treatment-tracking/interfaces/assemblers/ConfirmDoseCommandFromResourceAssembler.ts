c546834f3d6b9b9f53d8630378ac434fef830ae5
Mr-Code-start <vitalybaca92@gmail.com>
2026-05-29 21:55:46 -0500

    fix: ConfirmDoseCommandFromResourceAssembler and ConfirmDoseResource

--- src/context/treatment-tracking/interfaces/assemblers/ConfirmDoseCommandFromResourceAssembler.ts
+++ src/context/treatment-tracking/interfaces/assemblers/ConfirmDoseCommandFromResourceAssembler.ts
@@ -7,10 +7,8 @@ export class ConfirmDoseCommandFromResourceAssembler {
         resource: ConfirmDoseResource
     ): ConfirmDoseCommand {
         return {
-            treatmentId: resource.treatmentId,
             patientId: resource.patientId,
             motherId: resource.motherId,
-            dailyDoseId: resource.dailyDoseId
         };
     }
 }
